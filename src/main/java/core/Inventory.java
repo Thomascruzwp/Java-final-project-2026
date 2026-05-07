88d9fb3da0d0c8e4f948d841b425fc06f375d7a6
Thomascruzwp <cruztommy546@gmail.com>
2026-05-07 00:14:58 +0000

    added Inventory

---
+++ src/main/java/core/Inventory.java
@@ -0,0 +1,21 @@
+package core;
+
+import java.util.HashMap;
+import java.util.Map;
+import product.*;
+
+public class Inventory{
+    private Map<ProductSlot, Product> products = new HasMap<>();
+
+    public void addProduct(ProductSlot slot, Product product){
+        products.put(slot, product);
+    }
+
+    public Product getProduct(ProductSlot slot) {
+        return products.get(slot);
+    }
+
+    public Map<ProductSlot, Product> getProducts(){
+        return products;
+    }
+}
